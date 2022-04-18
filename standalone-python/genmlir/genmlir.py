ace8200e523fa43e6fcdcf75835a8e50ce498228
Joe Hendrix <joe.hendrix@sri.com>
2022-04-18 15:52:23 -0700

    Additional python support.

--- standalone-python/genmlir/genmlir.py
+++ standalone-python/genmlir/genmlir.py
@@ -1,7 +1,5 @@
 import ast
-from hashlib import new
 import sys
-from pprint import pprint
 
 import mlir_python.ir as mlir
 from mlir_python.dialects import (
@@ -22,10 +20,12 @@ class Analyzer(ast.NodeVisitor):
         self.block = None
         self.map = None
         self.onDone = None
+        self.exceptBlock = None
 
     # Internal support
 
-    def undef_value(self):
+    def undef_value(self, e: ast.AST):
+        sys.stderr.write(f'Unsupported value {e.__class__.__name__} at {e.lineno}:{e.col_offset}\n')
         with mlir.InsertionPoint(self.block):
             return python_d.UndefinedOp()
 
@@ -44,17 +44,38 @@ class Analyzer(ast.NodeVisitor):
         with mlir.InsertionPoint(self.block):
             return python_d.GetMethod(w, mlir.StringAttr.get(name))
 
+
+    def getExceptBlock(self):
+        if self.exceptBlock != None:
+           return self.exceptBlock
+
+        valueType = python_d.ValueType.get()
+        exceptBlock = self.block.create_after(valueType)
+        with mlir.InsertionPoint(exceptBlock):
+            python_d.Throw(exceptBlock.arguments[0])
+
+        self.exceptBlock = exceptBlock
+        return exceptBlock
+
     # Invoke the given method.
     def invoke(self, method: mlir.Value, args: list[Value], keywords=None):
         if keywords == None or len(keywords) == 0:
-            with mlir.InsertionPoint(self.block):
-                return python_d.Invoke(method, args, None)
+            keyAttr = None
         else:
             keyAttrs = []
             for k in keywords:
                 keyAttrs.append(mlir.StringAttr.get(k))
-            with mlir.InsertionPoint(self.block):
-                return python_d.Invoke(method, args, mlir.ArrayAttr.get(keyAttrs))
+            keyAttr = mlir.ArrayAttr.get(keyAttrs)
+
+        valueType = python_d.ValueType.get()
+        exceptBlock = self.getExceptBlock()
+        returnBlock = self.block.create_after(valueType)
+
+        with mlir.InsertionPoint(self.block):
+            python_d.Invoke(method, args, keyAttr, [], [], returnBlock, exceptBlock)
+
+        self.block = returnBlock
+        return returnBlock.arguments[0]
 
     # Invoke format value
     def format_value(self, v: Value, format: Value):
@@ -87,11 +108,17 @@ class Analyzer(ast.NodeVisitor):
 
     def int_constant(self, c: int):
         with mlir.InsertionPoint(self.block):
-            return python_d.IntLit(mlir.IntegerAttr.get(mlir.IntegerType.get_signed(64), c))
+            if -2**63 <= c and c < 2**63:
+                return python_d.S64Lit(mlir.IntegerAttr.get(mlir.IntegerType.get_signed(64), c))
+            else:
+                return python_d.IntLit(mlir.StringAttr.get(str(c)))
+
+    def builtin(self, name: str):
+        with mlir.InsertionPoint(self.block):
+            return python_d.Builtin(mlir.StringAttr.get(name))
 
     def load_value_attribute(self, v: Value, attr: str):
-        with mlir.InsertionPoint(self.block):
-            get = python_d.Builtin(mlir.StringAttr.get("getattr"))
+        get = self.builtin("getattr")
         return self.invoke(get, [v, self.string_constant(attr)])
 
     # Expressions
@@ -112,8 +139,42 @@ class Analyzer(ast.NodeVisitor):
         else:
             raise Exception(f'Unknown context {type(a.ctx)}')
 
-    def visit_BinOp(self, s: ast.BinOp):
-        return self.undef_value() # FIXME
+    # FIXME Make static
+    operator_map = {
+        ast.Add: python_d.AddOp,
+        ast.BitAnd: python_d.BitAndOp,
+        ast.BitOr: python_d.BitOrOp,
+        ast.BitXor: python_d.BitXorOp,
+        ast.Div: python_d.DivOp,
+        ast.FloorDiv: python_d.FloorDivOp,
+        ast.LShift: python_d.LShiftOp,
+        ast.Mod: python_d.ModOp,
+        ast.Mult: python_d.MultOp,
+        ast.MatMult: python_d.MatMultOp,
+        ast.Pow: python_d.PowOp,
+        ast.RShift: python_d.RShiftOp,
+        ast.Sub: python_d.SubOp
+    }
+
+    def apply_binop(self, left: mlir.Value, op, right: mlir.Value):
+        valueType = python_d.ValueType.get()
+        exceptBlock = self.getExceptBlock()
+        returnBlock = self.block.create_after(valueType)
+
+        with mlir.InsertionPoint(self.block):
+            op(left, right, [], [], returnBlock, exceptBlock)
+
+        self.block = returnBlock
+        return returnBlock.arguments[0]
+
+    def visit_BinOp(self, e: ast.BinOp):
+        op = self.operator_map.get(e.op.__class__)
+        if op == None:
+            return self.undef_value(e)
+
+        left  = self.checked_visit_expr(e.left)
+        right = self.checked_visit_expr(e.right)
+        return self.apply_binop(left, op, right)
 
     def visit_Call(self, c: ast.Call):
         f = self.visit(c.func)
@@ -130,14 +191,41 @@ class Analyzer(ast.NodeVisitor):
             keywords.append(k.arg)
         return self.invoke(f, args, keywords)
 
-    def visit_Compare(self, node: ast.Compare):
-        return self.undef_value() # FIXME
+
+    # FIXME Make static
+    comparison_map = {
+        ast.Eq : python_d.EqOp,
+        ast.Gt : python_d.GtOp,
+        ast.GtE : python_d.GtEOp,
+        ast.In : python_d.InOp,
+        ast.Is : python_d.IsOp,
+        ast.IsNot : python_d.IsNotOp,
+        ast.Lt : python_d.LtOp,
+        ast.LtE : python_d.LtEOp,
+        ast.NotEq : python_d.NotEqOp,
+        ast.NotIn : python_d.NotInOp
+    }
+
+    def visit_Compare(self, e: ast.Compare):
+        left  = self.checked_visit_expr(e.left)
+        args = e.comparators.__iter__()
+        for cmpop in e.ops:
+            right = self.checked_visit_expr(args.__next__())
+            op = self.comparison_map.get(cmpop.__class__)
+            if op == None:
+                left = self.undef_value(e)
+            else:
+                left = self.apply_binop(left, op, right)
+        return left
 
     def visit_Constant(self, c: ast.Constant):
         if isinstance(c.value, str):
             return self.string_constant(c.value)
         elif isinstance(c.value, int):
-            return self.int_constant(c.value)
+            r = self.int_constant(c.value)
+            if r == None:
+                raise Exception(f'Integer constant {c.value} at {c.lineno}:{c.col_offset} is out of range.')
+            return r
         else:
             raise Exception(f'Unknown Constant {c.value}')
 
@@ -152,7 +240,7 @@ class Analyzer(ast.NodeVisitor):
         return self.format_value(v, format)
 
     def visit_GeneratorExp(self, node: ast.GeneratorExp) -> mlir.Value:
-        return self.undef_value() # FIXME
+        return self.undef_value(node) # FIXME
 
     # See https://www.python.org/dev/peps/pep-0498/
     def visit_JoinedStr(self, s: ast.JoinedStr):
@@ -166,20 +254,47 @@ class Analyzer(ast.NodeVisitor):
                 raise Exception(f'Join expression expected constant or formatted value.')
         return self.joined_string(args)
 
-    def visit_List(self, s: ast.List):
-        return self.undef_value() # FIXME
+    def visit_Lambda(self, e: ast.Lambda) -> mlir.Value:
+        return self.undef_value(e) # FIXME
 
-    def visit_ListComp(self, s: ast.ListComp):
-        return self.undef_value() # FIXME
+    def visit_List(self, e: ast.List):
+        args = map(self.checked_visit_expr, e.elts)
+        with mlir.InsertionPoint(self.block):
+            return python_d.List(args)
+
+    def visit_ListComp(self, e: ast.ListComp):
+        return self.undef_value(e) # FIXME
 
     def visit_Name(self, node: ast.Name):
         return self.name_value(node.id)
 
-    def visit_Subscript(self, node: ast.Subscript):
-        return self.undef_value() #FIXME
+    def visit_Slice(self, e: ast.Slice):
+        upper = self.checked_visit_expr(e.upper) if e.upper != None else self.none_value()
 
-    def visit_Tuple(self, node: ast.Subscript):
-        return self.undef_value() #FIXME
+        sliceFn = self.builtin('slice')
+        if e.step != None:
+            lower = self.checked_visit_expr(e.lower) if e.lower != None else self.none_value()
+            step = self.checked_visit_expr(e.step)
+            return self.invoke(sliceFn, [lower, upper, step])
+        elif e.lower != None:
+            lower = self.checked_visit_expr(e.lower)
+            return self.invoke(sliceFn, [lower, upper])
+        else:
+            return self.invoke(sliceFn, [upper])
+
+    def visit_Subscript(self, e: ast.Subscript):
+        v = self.checked_visit_expr(e.value)
+        slice = self.checked_visit_expr(e.slice)
+        getitem = self.get_method(v, '__getitem__')
+        return self.invoke(getitem, [slice])
+
+    def visit_Tuple(self, e: ast.Tuple):
+        args = map(self.checked_visit_expr, e.elts)
+        with mlir.InsertionPoint(self.block):
+            return python_d.Tuple(args)
+
+#    def visit_UnaryOp(self, e: ast.UnaryOp) -> mlir.Value:
+#        return self.undef_value(e) # FIXME
 
     # Statements
     def checked_visit_stmt(self, e: ast.stmt):
@@ -206,7 +321,7 @@ class Analyzer(ast.NodeVisitor):
         elif isinstance(tgt, ast.Name):
             self.assign_name(tgt.id, v)
         else:
-            raise Exception(f'Unexpected target {tgt.__class__.__name__}')
+            raise Exception(f'Unexpected target {tgt.__class__.__name__} {tgt.lineno}:{tgt.col_offset}')
 
     def visit_Assign(self, a: ast.Assign):
         if len(a.targets) != 1:
@@ -225,12 +340,45 @@ class Analyzer(ast.NodeVisitor):
         return True
 
     def visit_For(self, s: ast.For) -> bool:
-        r = self.checked_visit_expr(s.target)
-
         if len(s.orelse) > 0:
-            raise Exception('For loop with else clause not yet supported.')
+            raise Exception('For orelse unsupported.')
+        r = self.checked_visit_expr(s.iter)
+        enter = self.get_method(r, '__iter__')
+        i = self.invoke(enter, [])
+        next = self.get_method(i, '__next__')
 
-        return True # FIXME
+        nextBlock = self.block.create_after()
+
+        with mlir.InsertionPoint(self.block):
+            cf_d.BranchOp([], nextBlock)
+
+        valueType = python_d.ValueType.get()
+
+        bodyBlock = nextBlock.create_after(valueType)
+        exceptBlock = bodyBlock.create_after(valueType)
+        with mlir.InsertionPoint(nextBlock):
+            python_d.Invoke(next, [], None, [], [], bodyBlock, exceptBlock)
+
+        # Get value for loop
+        value = bodyBlock.arguments[0]
+        bodyAnalyzer = Analyzer(self.m)
+        bodyAnalyzer.block = bodyBlock
+        bodyAnalyzer.map = self.map
+        bodyAnalyzer.assignLhs(s.target, value)
+        bodyCont = bodyAnalyzer.visitStmts(s.body)
+        if bodyCont:
+            with mlir.InsertionPoint(bodyAnalyzer.block):
+                cf_d.BranchOp([], nextBlock)
+
+        exception = exceptBlock.arguments[0]
+        doneBlock = exceptBlock.create_after()
+        throwBlock = self.getExceptBlock()
+        with mlir.InsertionPoint(exceptBlock):
+            c = python_d.IsInstance(exception, mlir.StringAttr.get("StopIteration"))
+            cf_d.CondBranchOp(c, [], [exception], doneBlock, throwBlock)
+
+        self.block = doneBlock
+        return True
 
     def visit_FunctionDef(self, s: ast.FunctionDef):
         scopeType = python_d.ScopeType.get()
@@ -346,8 +494,35 @@ class Analyzer(ast.NodeVisitor):
                 func_d.ReturnOp([])
         return True
 
-    def visit_While(self, node: ast.While) -> bool:
-        return True # FIXME
+    def visit_While(self, s: ast.While) -> bool:
+        if len(s.orelse) > 0:
+            raise Exception('While orelse unsupported.')
+
+        testBlock = self.block.create_after()
+        with mlir.InsertionPoint(self.block):
+            cf_d.BranchOp([], testBlock)
+
+        testAnalyzer = Analyzer(self.m)
+        testAnalyzer.block = testBlock
+        testAnalyzer.map = self.map
+        c = testAnalyzer.truthy(testAnalyzer.checked_visit_expr(s.test))
+
+        bodyBlock = testAnalyzer.block.create_after()
+        doneBlock = bodyBlock.create_after()
+        with mlir.InsertionPoint(testAnalyzer.block):
+            cf_d.CondBranchOp(c, [], [], bodyBlock, doneBlock)
+
+        # Execute loop body
+        bodyAnalyzer = Analyzer(self.m)
+        bodyAnalyzer.block = bodyBlock
+        bodyAnalyzer.map = self.map
+        bodyCont = bodyAnalyzer.visitStmts(s.body)
+        if bodyCont:
+            with mlir.InsertionPoint(bodyAnalyzer.block):
+                cf_d.BranchOp([], testBlock)
+
+        self.block = doneBlock
+        return True
 
 def main():
     if len(sys.argv) != 2:
--- standalone-python/test/script-test/script-test.cpp
+++ standalone-python/test/script-test/script-test.cpp
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include "mlir/IR/BuiltinDialect.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Parser.h"
 
 #include "JavaScript/JavaScriptDialect.h"
 #include "Python/PythonDialect.h"
 
-int main(int argc, char** argv) {
+void testJavascript() {
   mlir::MLIRContext context;
   context.loadDialect<mlir::js::JavaScriptDialect>();
   mlir::OwningOpRef<mlir::ModuleOp> owning =
@@ -17,8 +18,44 @@ int main(int argc, char** argv) {
 
   if (!owning) {
     printf("ERROR: Could not parse.\n");
-    return 1;
+    exit(-1);
+  }
+}
+
+
+void testPython() {
+  mlir::MLIRContext context;
+  context.loadDialect<mlir::cf::ControlFlowDialect>();
+  context.loadDialect<mlir::func::FuncDialect>();
+  context.loadDialect<mlir::python::PythonDialect>();
+  mlir::OwningOpRef<mlir::ModuleOp> owning =
+      mlir::parseSourceString(
+"module {\n"
+"  func @script_main() {\n"
+"    %0 = python.undefined : !python.value\n"
+"    %1 = python.truthy %0 : i1\n"
+"    python.invoke %0(), ^bb1, ^bb2\n"
+"  ^bb1(%a: !python.value):  // pred: ^bb0\n"
+"    return\n"
+"  ^bb2(%e: !python.value):  // pred: ^bb0\n"
+"    %c = python.isinstance %e, \"StopIteration\" : i1\n"
+"    cf.cond_br %c, ^bb3, ^bb4\n"
+"  ^bb3:  // pred: ^bb2\n"
+"    return\n"
+"  ^bb4:  // pred: ^bb2\n"
+"    python.throw %e\n"
+"  }\n"
+"}\n"
+        , &context);
+  if (!owning) {
+    printf("ERROR: Could not parse.\n");
+    exit(-1);
   }
   owning->dump();
+}
+
+int main(int argc, char** argv) {
+  testJavascript();
+  testPython();
   return 0;
 }
